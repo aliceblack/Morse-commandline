0eb1bdf7d5f07de6977d43c2ba1b537c53563def
Alice Black <blackfluid44@gmail.com>
2017-12-24 20:00:06 +0100

    minigame guess the symbol

--- morse-app/app.js
+++ morse-app/app.js
@@ -1,8 +1,12 @@
 let code = require('./code');
 const readline = require('readline');
 
-console.log('Morse -- --- .-. ... .');
-console.log('Select your input method: message | letter | morse');
+console.log('\nMorse -- --- .-. ... . \n ');
+console.log('Select your input method: message | letter | morse | guess \n');
+console.log('message - convert a message to morse');
+console.log('letter - get its morse symbol');
+console.log('morse - get the corresponding letter');
+console.log('guess - write the correct symbol');
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -40,6 +44,18 @@ rl.on('line', (input) => {
     rl.on('line', (input) => {
       let k=Object.keys(code).find(key => code[key] === input);
       console.log(k);
+    });
+  }
+  else if(input=='guess'){
+    var keys = Object.keys(code);
+    let randomKey = keys[ keys.length * Math.random() << 0];
+    console.log(randomKey);
+    rl.on('line', (input) => {
+      input=input.toUpperCase();
+      let userAnswer=Object.keys(code).find(key => code[key] === input);
+      console.log('Your answer is:   '+input+'   '+userAnswer);
+      let correctLetter = code[randomKey];
+      console.log('The correct answer is:   '+correctLetter+' '+ randomKey);
     });
 
   }
